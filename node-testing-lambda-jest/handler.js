1fb48abcce1d2749a5e1f0cd2cb52efd648d1639
Marcelo Wis <marcelowisc@gmail.com>
2020-12-01 21:51:35 -0300

    remove getLocalGreeting

--- node-testing-lambda-jest/handler.js
+++ node-testing-lambda-jest/handler.js
@@ -3,30 +3,9 @@
 const OrderController = require("./controllers/OrderController");
 
 
-function getLocalGreeting(language) {
-  switch(language) {
-    case "en":
-      return "Hello!";
-    case "es":
-      return "¡Hola!";
-    case "ru":
-      return "Привет!";
-    default:
-      return "🌊";
-  }
-}
-
-function pickLocale() {
-  const languages = ["en", "es", "cn", "fr", "ru"];
-
-  return languages [Math.floor(Math.random() * languages.length)];
-}
-
 module.exports.endpoint = async (event, context, callback) => {
   const response = await OrderController.post(event)
 
   callback(null, response);
 };
 
-
-  module.exports.getLocalGreeting = getLocalGreeting;
